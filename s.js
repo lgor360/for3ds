f9c02c6318ca28559b9c9c29eef2c4527dfb286f
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-25 17:32:03 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,6 @@
 alert('привет! я тебя помню! ты с моего другого сайта! может с homescreenlite? я кстати Игорь. создатель этого сайта. ну или оцелот :3')
-let TSelect = document.querySelector("[name='tselect']");
-TSelect.value = LoadSelectValue(TSelect);
+let ThSelect = document.querySelector("[name='thselect']");
+ThSelect.value = LoadSelectValue(TSelect);
 function LoadSelectValue(el) {
   return localStorage.getItem(el.name);
 }
