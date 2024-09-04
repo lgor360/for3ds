6d97644d051bc1dcca38dcee4d0852a78157713b
Игорь <93474580+lgor360@users.noreply.github.com>
2024-09-04 11:42:12 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,9 +1,7 @@
 if (navigator.platform === "New Nintendo 3DS") {
   console.log('о супер у тебя консоль семейтсва 3DS :D');
-  load();
 } else if (navigator.platform === "Nintendo 3DS") {
   console.log('о супер у тебя консоль семейтсва 3DS :D');
-  load();
 } else {
   window.location.href = "/igor360website/home.html";
 }
