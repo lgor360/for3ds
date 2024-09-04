3002ad5bfcfe66354270480fe031e0d77b6c5091
Игорь <93474580+lgor360@users.noreply.github.com>
2024-09-04 11:28:41 +0300

    Update s.js

--- s.js
+++ s.js
@@ -7,18 +7,3 @@ if (navigator.platform === "New Nintendo 3DS") {
 } else {
   window.location.href = "/igor360website/home.html";
 }
-function load() {
-  alert("привет! это моя игра на html!");
-  alert("я не знаю на каком языке можно делать игры чтобы их можно было потом установить как прогу на 3ds... я уже пробывал LOVE или же Love2D, но у меня не получилось... так что я буду использовать html!");
-  hi();
-}
-function hi() {
-  const name = prompt('для начала введи свой никнейм');
-  if (name) {
-    let hi = "прикольный ник, " += name
-    alert(hi);
-  } else {
-    alert("ты ничего не ввёл!");
-    hi();
-  }
-}
